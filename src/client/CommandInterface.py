c61dc5be6ad31616da9c49328add05ed2aee8eee
Adam Bellemare <betastas@gmail.com>
2014-05-15 08:59:54 -0400

    Added DataManager as a singleton class, converted MonitorManager to a singleton, and added CommandInterface to be used to route server commands. Modified tests for each class too.

---
+++ src/client/CommandInterface.py
@@ -0,0 +1,10 @@
+
+## Receives commands from the server and executes them.
+#
+class CommandExecutor():
+
+    def __init__(self):
+        self._state = "Unimplemented"
+
+
+    def execute_command(self):
